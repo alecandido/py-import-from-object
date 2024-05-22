2b532f247fa1f8201b7698258cf40c67e9fa804b
Alessandro Candido <candido.ale@gmail.com>
2024-05-23 00:07:49 +0200

    build: Init package

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.devenv.flakeModule];
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = self;
+          preferWheels = true;
+        };
+
+        devenv.shells.default = {
+          packages = with pkgs; [pre-commit poethepoet];
+
+          languages.python = {
+            enable = true;
+            poetry = {
+              enable = true;
+              install = {
+                enable = true;
+                groups = ["dev"];
+              };
+            };
+          };
+        };
+      };
+    };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+}

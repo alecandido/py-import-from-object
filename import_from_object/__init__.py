1a53157248bda74002ecab97daa00f0b8b240eb6
Alessandro Candido <candido.ale@gmail.com>
2024-05-24 11:44:47 +0200

    feat: Add default object instance

--- import_from_object/__init__.py
+++ import_from_object/__init__.py
@@ -1,30 +1,3 @@
-import sys
-from functools import partial
+from .container import ModuleContainer
 
-from . import assets
-
-
-class ModuleContainer:
-    def __init__(self, name) -> None:
-        self.name = name
-        if name in sys.modules:
-            raise ValueError(
-                f"Module '{name}' already present. "
-                "Choose a different one to avoid overwriting it."
-            )
-        sys.modules[name] = self
-
-    def __getattribute__(self, name):
-        mname = super().__getattribute__("name")
-        attrs = {
-            "__spec__": None,
-            "__name__": mname,
-        }
-
-        try:
-            return attrs[name]
-        except KeyError:
-            attr = getattr(assets, name)
-            if not callable(attr):
-                return attr
-            return partial(attr, mname)
+ModuleContainer("import_from_object.generated")
---
+++ tests/simple.py
@@ -0,0 +1,4 @@
+from import_from_object.generated import ciao, come
+
+ciao()
+come()
